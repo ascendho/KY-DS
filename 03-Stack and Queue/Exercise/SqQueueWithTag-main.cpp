1589375fbcc45013ad409d411f143755ffbb7435
Ascend Ho <coderhs@foxmail.com>
2023-07-05 22:44:08 +0800

    3.7-SqQueueWithTag

---
+++ 03-Stack and Queue/Exercise/SqQueueWithTag-main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "SqQueueWithTag.h"
+
+using namespace std;
+
+
+int main() {
+    SqQueue Q;
+    InitQueue(Q);
+
+    int e = 1;
+    EnQueue(Q, e);
+
+    char t;
+    DeQueue(Q, t);
+
+    cout << t << endl;
+    return 0;
+}
---
+++ 03-Stack and Queue/Exercise/SqQueueWithTag.cpp
@@ -0,0 +1,50 @@
+#include "SqQueueWithTag.h"
+#include <iostream>
+
+// 构造一个空队列Q
+Status InitQueue(SqQueue &Q) {
+    Q.base = new QElemType[MAXQSIZE];
+    // 存储分配失败
+    if (!Q.base) exit(OVERFLOW);
+
+    // 头指针和尾指针置为零，队列为空
+    Q.front = Q.rear = 0;
+
+    // 标志初始化为0，队列为空
+    Q.tag = 0;
+    return OK;
+}
+
+// 插入元素e为Q的新的队尾元素
+Status EnQueue(SqQueue &Q, QElemType e) {
+    // 队满
+    if ((Q.tag == 1) && (Q.rear == Q.front))
+        return ERROR;
+
+    // 新元素插入队尾
+    Q.base[Q.rear] = e;
+
+    // 尾指针加1
+    Q.rear = (Q.rear + 1) % MAXQSIZE;
+
+    // 标志改1，表示队列非空
+    if (Q.tag == 0) Q.tag = 1;
+    return OK;
+}
+
+// 删除Q的队头元素，用e返回其值
+Status DeQueue(SqQueue &Q, QElemType &e) {
+    // 队空
+    if ((Q.tag == 0) && (Q.rear == Q.front))
+        return ERROR;
+
+    // 保存队头元素
+    e = Q.base[Q.front];
+
+    // 头指针加1
+    Q.front = (Q.front + 1) % MAXQSIZE;
+
+    // 标志改0，表示队列非满
+    if (Q.tag == 1) Q.tag = 0;
+    return OK;
+}
