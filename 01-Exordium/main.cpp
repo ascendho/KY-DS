41a5600e3df451e01f43ca827cbf07c473e81833
Ascend Ho <coderhs@foxmail.com>
2023-07-13 21:55:31 +0800

    Delete redundant column

--- 01-Exordium/main.cpp
+++ 01-Exordium/main.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main() {
 
+    cout << "This is a project built for DS" << endl;
+
     return 0;
 }
--- 05-Tree and Binary Tree/TextBook/SqBiTree.h
+++ 05-Tree and Binary Tree/TextBook/SqBiTree.h
@@ -4,7 +4,7 @@
 #include "../../Status/Status.h"
 
 // 二叉树的顺序存储表示
-typedef TElemType SqBiTree[MAXTSIZE]; //0号单元存储根节点
+typedef TElemType SqBiTree[MAXTSIZE]; // 0号单元存储根节点
 SqBiTree bt;
 
 #endif //TEXTBOOK_SQBITREE_H
