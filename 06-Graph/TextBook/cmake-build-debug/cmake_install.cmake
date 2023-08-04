f2cdb07cd5f0aab7e2024aa8d9edb75efa95363f
Ascend Ho <coderhs@foxmail.com>
2023-08-04 22:42:04 +0800

    fix name issue

--- 06-Graph/TextBook/cmake-build-debug/cmake_install.cmake
+++ 06-Graph/TextBook/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/data/duotone/repo/06- Graph/TextBook
+# Install script for directory: D:/data/duotone/repo/06-Graph/TextBook
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/data/duotone/repo/06- Graph/TextBook/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/data/duotone/repo/06-Graph/TextBook/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
