6368d6c58559f3c5e8c8fe346e1eb3138024b18e
Ascend Ho <coderhs@foxmail.com>
2023-08-07 21:41:29 +0800

    Kruskal algorithm

--- 06-Graph/TextBook/MinimumCostSpanningTree.cpp
+++ 06-Graph/TextBook/MinimumCostSpanningTree.cpp
@@ -59,3 +59,59 @@ int Min(AMGraph G) {
 
     return index;
 }
+
+// 算法6.9　克鲁斯卡尔算法
+// 无向网G以邻接矩阵形式存储，构造G的最小生成树T，输出T的各条边
+
+void MiniSpanTree_Kruskal(AMGraph G) {
+    int v1, v2, vs1, vs2;
+
+    Sort(G);                                                  // 将数组Edge中的元素按权值从小到大排序
+    for (int i = 0; i < G.vexnum; ++i)                        // 辅助数组，表示各顶点自成一个连通分量
+        Vexset[i] = i;
+
+    // 依次查看排好序的数组Edge中的边是否在同一连通分量上
+    for (int i = 0; i < G.arcnum; ++i) {
+
+        v1 = LocateVex(G, Edge[i].Head);                // v1为边的始点Head的下标
+        v2 = LocateVex(G, Edge[i].Tail);                // v2为边的终点Tail的下标
+        vs1 = Vexset[v1];                                  // 获取边Edge[i]的始点所在的连通分量vs1
+        vs2 = Vexset[v2];                                  // 获取边Edge[i]的终点所在的连通分量vs2
+
+        // 边的两个顶点分属不同的连通分量
+        if (vs1 != vs2) {
+            std::cout << Edge[i].Head << "-->" << Edge[i].Tail << std::endl;        // 输出此边
+            for (int j = 0; j < G.vexnum; ++j)                                      // 合并vs1和vs2两个分量，即两个集合统一编号
+                if (Vexset[j] == vs2) Vexset[j] = vs1;                              // 集合编号为vs2的都改为vs1
+        }
+    }
+}
+
+// 冒泡排序
+void Sort(AMGraph G) {
+    int m = G.arcnum - 2;
+    int flag = 1;
+
+    while ((m > 0) && flag == 1) {
+        flag = 0;
+        for (int j = 0; j <= m; j++) {
+            if (Edge[j].lowcost > Edge[j + 1].lowcost) {
+                flag = 1;
+
+                VerTexType temp_Head = Edge[j].Head;
+                Edge[j].Head = Edge[j + 1].Head;
+                Edge[j + 1].Head = temp_Head;
+
+
+                VerTexType temp_Tail = Edge[j].Tail;
+                Edge[j].Tail = Edge[j + 1].Tail;
+                Edge[j + 1].Tail = temp_Tail;
+
+                ArcType temp_lowcost = Edge[j].lowcost;
+                Edge[j].lowcost = Edge[j + 1].lowcost;
+                Edge[j + 1].lowcost = temp_lowcost;
+            }
+        }
+        --m;
+    }
+}
