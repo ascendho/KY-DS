3e1e93fc3690a722827f163c9603d3adfee6c518
Ascend Ho <coderhs@foxmail.com>
2023-08-21 20:29:29 +0800

    code review

--- 03-Stack_Queue/Applied Case/Matching/Matching.h
+++ 03-Stack_Queue/Applied Case/Matching/Matching.h
@@ -1,7 +1,7 @@
 #ifndef INC_03_STACK_AND_QUEUE_MATCHING_H
 #define INC_03_STACK_AND_QUEUE_MATCHING_H
 
-#include "../../TextBook/LinkStack.h"
+#include "../../TextBook/02_LinkStack/LinkStack.h"
 #include "../../../Status/Status.h"
 
 Status Matching();
