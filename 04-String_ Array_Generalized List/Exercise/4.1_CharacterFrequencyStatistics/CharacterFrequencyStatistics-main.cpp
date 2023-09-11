971c49de580e19ee337336ee08a5e78dffcb7e34
Ascend Ho <coderhs@foxmail.com>
2023-09-11 20:56:14 +0800

    fix exercise 4.1

--- 04-String_ Array_Generalized List/Exercise/4.1_CharacterFrequencyStatistics/CharacterFrequencyStatistics-main.cpp
+++ 04-String_ Array_Generalized List/Exercise/4.1_CharacterFrequencyStatistics/CharacterFrequencyStatistics-main.cpp
@@ -1,6 +1,9 @@
 #include "CharacterFrequencyStatistics.h"
+#include <iostream>
 
 int main() {
+    system("chcp 65001");
+
     Count();
     return 0;
 }
--- 04-String_ Array_Generalized List/Exercise/4.1_CharacterFrequencyStatistics/CharacterFrequencyStatistics.cpp
+++ 04-String_ Array_Generalized List/Exercise/4.1_CharacterFrequencyStatistics/CharacterFrequencyStatistics.cpp
@@ -1,40 +1,36 @@
 #include "CharacterFrequencyStatistics.h"
 #include<iostream>
 
-using namespace std;
-
 // 统计输入字符串中数字字符和字母字符的个数
 void Count() {
-    int num[36];
+    int num[LENGTH];
     char ch;
-    int i;
 
     // 初始化
-    for (i = 0; i < 36; i++) {
+    for (int i = 0; i < LENGTH; i++) {    // LENGTH为总长度36(即大写字母个数加数字个数)
         num[i] = 0;
     }
 
     // 依次读入字符进行判断
     while ((ch = getchar()) != '\n') {
         // 数字字符
-        if ('0' <= ch <= '9') {
-            i = ch - 48;
+        if ('0' <= ch && ch <= '9') {
+            int i = ch - 48;
             num[i]++;
-        } else if ('A' <= ch <= 'Z')    // 字母字符
+        } else if ('A' <= ch && ch <= 'Z')    // 字母字符
         {
-            i = ch - 65 + 10;
+            int i = ch - 65 + 10;
             num[i]++;
         }
     }
 
-    system("chcp 65001");
-
-    for (i = 0; i < 10; i++)    // 输出数字字符的个数
-    {
-        cout << "数字" << i << "的个数=" << num[i] << endl;
+    // 输出数字字符的个数
+    for (int i = 0; i < NUMLENGTH; i++) {          // NUMLENGTH为数字长度10
+        std::cout << "数字" << i << "的个数=" << num[i] << std::endl;
     }
-    for (i = 10; i < 36; i++)    // 输出字母字符的个数
-    {
-        cout << "字母字符" << char(i + 55) << "的个数=" << num[i] << endl;
+
+    // 输出字母字符的个数
+    for (int i = NUMLENGTH; i < LENGTH; i++) {
+        std::cout << "字母字符" << char(i + 55) << "的个数=" << num[i] << std::endl;
     }
 }
