2aad7c697a397130c339c9abb9fc9c8ec4b65375
Ascend Ho <coderhs@foxmail.com>
2023-09-11 20:27:31 +0800

    more complete dp solution for exercise4.6

--- 04-String_ Array_Generalized List/Exercise/4.6_Partition/Partition-main.cpp
+++ 04-String_ Array_Generalized List/Exercise/4.6_Partition/Partition-main.cpp
@@ -4,14 +4,18 @@
 void Print(int A[], int length);
 
 int main() {
-    int arr[] = { 1 , 3,-1,-2,8,9,-7,5,-1,2,6,-7,-2 };
-    int num = sizeof(arr) / sizeof(arr[0]);     // 获取数组长度
+    int arr1[] = {1, 3, -1, -2, 8, 9, -7, 5, -1, 2, 6, -7, -2};
+    int arr2[] = {3, 1, -1, -3, 4, 5, -2};
 
-//    Partition(arr, num);
+    int length1 = sizeof(arr1) / sizeof(arr1[0]);     // 获取数组长度
+    int length2 = sizeof(arr2) / sizeof(arr2[0]);     // 获取数组长度
+
+    Partition_dp(arr1, length1);
+    Print(arr1, length1);
 
 
-    Partition_dp(arr,num);
-    Print(arr,num);
+    Partition_dp(arr2, length2);
+    Print(arr2, length2);
 
     return 0;
 }
@@ -22,5 +26,6 @@ void Print(int A[], int length) {
     for (int i = 1; i < length; i++) {
         std::cout << "->" << A[i];
     }
+
     std::cout << std::endl;
 }
--- 04-String_ Array_Generalized List/Exercise/4.6_Partition/Partition.cpp
+++ 04-String_ Array_Generalized List/Exercise/4.6_Partition/Partition.cpp
@@ -17,35 +17,27 @@ void Partition(int A[], int n) {
     }
 }
 
-// 双指针法 目前还存在问题
-void Partition_dp(int A[], int n){
-    int tag1 = 0, tag2 = 0;
-    int Neg, Pos;                  // 分别指向负数和正数
+// 双指针法 (初步验证无误)
+void Partition_dp(int A[], int n) {
+    int NegPtr = 0, PosPtr = 0;                     // 分别指向负数和正数, 初始化指向0
+    bool IsNegative = false, IsPositive = false;    // 分别标志双指针是否分别指向负数和正数, 初始化为false
 
-    for (int i = 0, j = 0; i < n && j < n;) {
-        if (A[i] < 0 && tag1 == 0) {
-            Neg = i;
-            tag1 = 1;
-        }
+    // 负数和正数指针都不能越界
+    while (NegPtr < n && PosPtr < n) {
+        if (A[NegPtr] < 0 ) IsNegative = true;   // 验证负数指针是否指向负数
+        if (A[PosPtr] > 0 ) IsPositive = true;   // 验证正数指针是否指向正数
 
-        if (A[j] > 0 && tag2 == 0) {
-            Pos = j;
-            tag2 = 1;
-        }
-
-        if (tag1 == 1 && tag2 == 1) {
-            if (Neg < Pos) {
-                std::swap(A[Neg], A[Pos]);
-                tag1 = tag2 = 0;
+        // 分别指向负数和正数时
+        if (IsNegative && IsPositive) {
+            if (NegPtr < PosPtr) {                      // 负数在整数前面
+                std::swap(A[NegPtr], A[PosPtr]);  // 交换
+                IsNegative = IsPositive = false;        // 调整标志值以便移动指针
             } else {
-                tag2=0;
+                IsPositive = false;                     // 负数在正数后面, 令标志值为false以便移动指针
             }
         }
 
-        if (tag1 == 0) i++;
-        if (tag2 == 0) j++;
+        if (!IsNegative) NegPtr++;         // 移动负数指针
+        if (!IsPositive) PosPtr++;         // 移动正数指针
     }
 }
-
-
-// 3, 1, -1, -3, 4, 5, -2
